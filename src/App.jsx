4231f8127bd097b3bfe9e00812eb7bde46cd12dd
Breanna <b.gonzales21@yahoo.com>
2023-09-26 19:48:42 -0700

    added

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import './App.css'
+
+<div id="container">
+<h1>Hello React Router!</h1>
+<div id="navbar">{/* navigation here */}</div>
+<div id="main-section">{/* routes here */}</div>
+</div>
+
+export default App
